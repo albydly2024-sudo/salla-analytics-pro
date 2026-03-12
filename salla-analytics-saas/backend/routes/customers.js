b943b7079f836445b657a2bf559e696bf9193fb1
Salla Admin <admin@salla-saas.local>
2026-03-12 03:14:41 +0300

    chore: purge cached backend tracking

--- salla-analytics-saas/backend/routes/customers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const customersController = require('../controllers/customers');
-
-router.get('/', customersController.getCustomers);
-
-module.exports = router;
--- salla-analytics-saas/backend/routes/orders.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ordersController = require('../controllers/orders');
-
-router.get('/', ordersController.getOrders);
-
-module.exports = router;
